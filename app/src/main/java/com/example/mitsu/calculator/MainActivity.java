a22cc6587f81d2dbb00b227a00f7de0ea3716878
Mitsuki-Sakamoto <mitsuki.sakamoto@happyhack.co.jp>
2018-05-21 13:47:28 +0900

    First revise

--- app/src/main/java/com/example/mitsu/calculator/MainActivity.java
+++ app/src/main/java/com/example/mitsu/calculator/MainActivity.java
@@ -3,150 +3,177 @@ package com.example.mitsu.calculator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.math.BigDecimal;
-import java.util.concurrent.Callable;
 
 public class MainActivity extends AppCompatActivity {
-    private boolean f;
-    private int Length;
+    private boolean input;
+    private int length;
     private int operator;
-    private boolean setOperator=false,finishOperate,setOperand;
-    private BigDecimal x,y,result,zero;
-    boolean over;
-    TextView Answer;
-    ImageButton N;
+    private boolean isSetOperator,finishOperate;
+    private BigDecimal firstOperator,secondOperator,calculationResult;
+    private final BigDecimal ZERO = new BigDecimal(0);
+    private boolean isOverflow;
+    TextView answer;
+    ImageButton button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Answer = findViewById(R.id.Answer);
-        zero=new BigDecimal("0");
+        answer = findViewById(R.id.answer);
         initCalculator();
     }
     public void initCalculator(){
-        f=true;
-        setOperator=false;finishOperate=true;setOperand=false;
-        x=y=result=zero;
-        Answer.setText(String.valueOf(0));
-        Length=Answer.getText().length();
-        over=false;
+        input=true;
+        isSetOperator=false;
+        finishOperate=true;
+        firstOperator=ZERO;
+        secondOperator=ZERO;
+        calculationResult=ZERO;
+        answer.setText("0");
+        length=1;
+        isOverflow=false;
     }
     public void initCalculator(View view){
-        f=true;
-        setOperator=false;finishOperate=true;setOperand=false;
-        x=y=result=zero;
-        Answer.setText(String.valueOf(0));
-        Length=0;
-        over=false;
+        initCalculator();
     }
 
-    public void CE(View view){
-        y=zero;
-        f=true;
-        Answer.setText(String.valueOf(0));
+    public void clearEntry(View view){
+        secondOperator=ZERO;
+        input=true;
+        answer.setText(String.valueOf(0));
     }
 
 
 
 
     public void ClickNumber(View view) {
-        double a,b,ans;
-        Answer = findViewById(R.id.Answer);
-        N = findViewById(view.getId());
-        String Sans=Answer.getText().toString();
-        String SN=N.getContentDescription().toString();
+        int numberId;
+        numberId=view.getId();
+        answer = findViewById(R.id.answer);
+        button = findViewById(numberId);
+        String answerString=answer.getText().toString();
+        String numberString=null;
+        switch (numberId){
+            case R.id.num0:
+                numberString="0";
+                break;
+            case R.id.num1:
+                numberString="1";
+                break;
+            case R.id.num2:
+                numberString="2";
+                break;
+            case R.id.num3:
+                numberString="3";
+                break;
+            case R.id.num4:
+                numberString="4";
+                break;
+            case R.id.num5:
+                numberString="5";
+                break;
+            case R.id.num6:
+                numberString="6";
+                break;
+            case R.id.num7:
+                numberString="7";
+                break;
+            case R.id.num8:
+                numberString="8";
+                break;
+            case R.id.num9:
+                numberString="9";
+                break;
+            case R.id.dot:
+                numberString=".";
+                break;
+        }
 
 
-                if (!f) {
-                    if ((N.getId() != R.id.Dot || !Sans.contains("."))&&Length < 9) {
+        if (!input) {
+            if ((numberId != R.id.dot || (!answerString.contains("."))&&length < 9)) {
+                answerString = answerString.concat(numberString);
+                answer.setText(answerString);
+            }
+        } else {
+            if (numberId != R.id.dot) answer.setText(numberString);
+            else answer.setText("0.");
+            if (numberId != R.id.num0) input = false;
+        }
+        length = answer.getText().length();
 
-                        Sans = Sans + SN;
-                        //Toast.makeText(MainActivity.this,Sans, Toast.LENGTH_LONG).show();
-                        Answer.setText(Sans);
-                    }
+        answerString = answer.getText().toString();
 
-
-                } else {
-                    if (N.getId() != R.id.Dot) Answer.setText(SN);
-                    else Answer.setText("0.");
-                    //Toast.makeText(MainActivity.this, N.getContentDescription(), Toast.LENGTH_LONG).show();
-                    if (N.getId() != R.id.Num0) f = false;
-                }
-                Length = Answer.getText().length();
-
-                Sans = Answer.getText().toString();
-                    if (!setOperator)
-                        x =new BigDecimal(Sans);
-                    else {
-                        y =new BigDecimal(Sans);
-                        setOperand = true;
-                    }
+        if (!isSetOperator)
+            firstOperator =new BigDecimal(answerString);
+        else {
+            secondOperator =new BigDecimal(answerString);
+            isSetOperator = true;
+        }
 
     }
 
     public void setOperator(View view){
-        if(!finishOperate&&setOperand){
-            Calsulate();
+        if(!finishOperate&&isSetOperator){
+            Calculate();
         }
-        N = findViewById(view.getId());
-        if(N.getId()==R.id.Add) operator=1;
-        if(N.getId()==R.id.Sub) operator=2;
-        if(N.getId()==R.id.Mult) operator=3;
-        if(N.getId()==R.id.Div) operator=4;
-        setOperator=true;
-        f=true;finishOperate=false;
+        button = findViewById(view.getId());
+        if(button.getId()==R.id.add) operator = 1;
+        else if(button.getId()==R.id.sub) operator = 2;
+        else if(button.getId()==R.id.mult) operator = 3;
+        else if(button.getId()==R.id.div) operator = 4;
+        isSetOperator = true;
+        input = true;
+        finishOperate=false;
     }
 
     public void clickEqual(View view){
-        Calsulate();
+        Calculate();
         finishOperate=true;
-        f=true;
+        input=true;
     }
 
-    public void Calsulate() {
+    public void Calculate() {
 
-        if (y.compareTo(zero) == 0 && operator == 4) {
-            Toast.makeText(MainActivity.this, "エラー", Toast.LENGTH_LONG).show();
+        if (secondOperator.compareTo(ZERO) == 0 && operator == 4) {
+            Toast.makeText(MainActivity.this, "#DIV/0!", Toast.LENGTH_LONG).show();
         }else {
             //Toast.makeText(MainActivity.this,("x"+String.valueOf(x)+"y"+String.valueOf(y)), Toast.LENGTH_LONG).show();
-            if (operator == 1) result = x.add(y);
-            if (operator == 2) result = x.subtract(y);
-            if (operator == 3) result = x.multiply(y);
-            if (operator == 4) result = x.divide(y, 8, BigDecimal.ROUND_HALF_UP);
+            if (operator == 1) calculationResult = firstOperator.add(secondOperator);
+            else if (operator == 2) calculationResult = firstOperator.subtract(secondOperator);
+            else if (operator == 3) calculationResult = firstOperator.multiply(secondOperator);
+            else if (operator == 4) calculationResult = firstOperator.divide(secondOperator, 8, BigDecimal.ROUND_HALF_UP);
 
-            x = result;
+            firstOperator = calculationResult;
 
-            Answer.setText(setR(result));
-            setOperator = false;
-            setOperand = false;
+            answer.setText(setResultString(calculationResult));
+            isSetOperator = false;
         }
     }
 
-    private String setR(BigDecimal result) {
-        String Sresult="0";
-        if(over){
-            Toast.makeText(MainActivity.this, "エラー", Toast.LENGTH_LONG).show();
+    private String setResultString(BigDecimal result) {
+        String resultString="0";
+        if(isOverflow){
+            Toast.makeText(MainActivity.this, "Over Flow!", Toast.LENGTH_LONG).show();
             initCalculator();
         }else {
             int i = result.precision() - result.scale();
             int j= result.precision();
             if (i > 9) {
                 result = result.setScale(-5, BigDecimal.ROUND_HALF_UP);
-                over = true;
+                isOverflow = true;
             } else if (j > 9) {
                 result = result.setScale(9 - i, BigDecimal.ROUND_HALF_UP);
             }
-            if(result.compareTo(zero)==0) result=zero;
+            if(result.compareTo(ZERO)==0) result=ZERO;
             if(result.scale()>0)result=result.stripTrailingZeros();
-            else Sresult = result.toString();
+            resultString = result.toString();
         }
-        return Sresult;
+        return resultString;
     }
 
 
